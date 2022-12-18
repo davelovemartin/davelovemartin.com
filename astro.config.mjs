75d126a8f77fb43b2e40e44afdd612d576480827
justan0therdave <justanotherdavemartin@gmail.com>
2022-12-18 09:47:00 +0000

    adding netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
+  output: "server",
+  adapter: netlify()
 });

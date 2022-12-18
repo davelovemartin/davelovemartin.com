fc8d26fbee4b04ae64a368513b97d6e63a45055b
justan0therdave <justanotherdavemartin@gmail.com>
2022-12-18 09:51:32 +0000

    removing typescript lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,6 @@ module.exports = {
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro", ".ts"],
-      },
       rules: {
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"

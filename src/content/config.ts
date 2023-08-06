dcc0bf1caf5443ec9d6d2e896a77a386419cc49a
davelovemartin <dave.lovemartin@immersivelabs.com>
2023-08-06 09:14:11 +0100

    changing experiements

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,27 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
-const thoughtsCollection = defineCollection({ 
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        stage: z.enum(['note','essay','reflection']),
-        lastEdited: z.string(),
-        theme: z.enum(['self-management','innovation','writing']),
-        draft: z.boolean().optional(),
-    })
- });
+const thoughtsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    stage: z.enum(["note", "essay", "reflection"]),
+    lastEdited: z.string(),
+    theme: z.enum(["self-management", "innovation", "writing"]),
+    draft: z.boolean().optional(),
+  }),
+});
+
+const experimentsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    lastEdited: z.string(),
+    type: z.enum(["thought", "code"]),
+    draft: z.boolean().optional(),
+  }),
+});
 
 export const collections = {
-  'thoughts': thoughtsCollection,
+  thoughts: thoughtsCollection,
+  experiments: experimentsCollection,
 };

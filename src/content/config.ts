74142ae254c8652587211b92cdb6610c1a3603f6
justan0therdave <justanotherdavemartin@gmail.com>
2024-02-29 16:38:29 +0000

    toggle notes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,27 @@
 import { z, defineCollection } from "astro:content";
 
+export const stages = ["note", "essay", "reflection"] as const;
+
+export type Stage = typeof stages[number];
+
+export const tags = ["code", "innovation", "self-management"] as const;
+
+export type Tag = typeof tags[number];
+
 const thoughtsCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    stage: z.enum(["note", "essay", "reflection"]),
+    stage: z.enum(stages),
     lastEdited: z.string(),
-    tag: z.enum(["self-management", "innovation", "writing"]),
+    tag: z.enum(tags),
     draft: z.boolean().optional(),
   }),
 });
 
 const experimentsCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
